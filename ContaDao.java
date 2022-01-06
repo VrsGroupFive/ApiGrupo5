ac21125c033039345cd7caf27a756e69d7d72ae0
RAFAELA DOS REIS FERREIRA <92344370+Rafaela202@users.noreply.github.com>
2022-01-06 18:51:32 -0300

    Deletado ContaDao

--- ContaDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.apigrupo5.api.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.apigrupo5.api.model.Conta;
-
-public interface ContaDao extends CrudRepository <Conta, Integer>{
-
-}
